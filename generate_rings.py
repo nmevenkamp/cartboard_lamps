cb3ac026a85dec215862e42d9c7ae4efc6f98b24
nmevenkamp <niklas.mevenkamp@gmail.com>
2021-07-31 08:38:53 +0200

    support multiple lamps

--- generate_rings.py
+++ generate_rings.py
@@ -1,107 +1,120 @@
+from typing import List
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-def compute_rings(
-    sphere_diameter: float,
-    layer_thickness: float,
-    ring_width: float,
-    sphere_opening_top: float,
-    sphere_opening_bottom: float
-) -> np.ndarray:
-    # start with center ring
-    r_o = sphere_diameter / 2
-    r_i = r_o - ring_width
-    res = [[0, r_i, r_o]]
+from utils import data_linewidth_plot
 
-    # top rings
-    z = 0
-    while r_i > sphere_opening_top / 2:
-        z += layer_thickness
-        r_o = np.sqrt((sphere_diameter / 2) ** 2 - z**2)
-        r_i = r_o - ring_width
-        res = [[z, r_i, r_o]] + res
-    
-    # bottom rings
-    z = 0
-    r_i = sphere_diameter / 2 - ring_width
-    while r_i > sphere_opening_bottom / 2:
-        z -= layer_thickness
-        r_o = np.sqrt((sphere_diameter / 2) ** 2 - z**2)
-        r_i = r_o - ring_width
-        res += [[z, r_i, r_o]]
 
-    # convert to numpy array and round to integers
-    res = np.asarray(res)
-    res = np.round(res)
+class LampParams:
+    def __init__(
+            self,
+            sphere_diameter=450,
+            layer_thickness=8,
+            ring_width=15,
+            sphere_opening_top=150,
+            sphere_opening_bottom=250
+    ):
+        self.sphere_diameter = sphere_diameter
+        self.layer_thickness = layer_thickness
+        self.ring_width = ring_width
+        self.sphere_opening_top = sphere_opening_top
+        self.sphere_opening_bottom = sphere_opening_bottom
 
-    # correct inner diameters for sufficient overlap
-    i = (len(res) - 1) // 2
-    for ii in range(1, i):
-        middle = int(np.ceil((res[ii - 1, 2] + res[ii - 1, 1]) / 2))
-        res[ii, 1] = min(res[ii, 1], middle)
-    for ii in range(i + 1, len(res) - 1):
-        middle = int(np.ceil((res[ii + 1, 2] + res[ii + 1, 1]) / 2))
-        res[ii, 1] = min(res[ii, 1], middle)
 
+def compute_rings(lamps: List[LampParams]) -> np.ndarray:
+    lamp_arrays = []
+    for lamp_idx, lamp_params in enumerate(lamps):
+        # start with center ring
+        r_o = lamp_params.sphere_diameter / 2
+        r_i = r_o - lamp_params.ring_width
+        res = [[0, r_i, r_o, lamp_idx]]
+
+        # top rings
+        z = 0
+        while r_i > lamp_params.sphere_opening_top / 2:
+            z += lamp_params.layer_thickness
+            r_o = np.sqrt((lamp_params.sphere_diameter / 2) ** 2 - z ** 2)
+            r_i = r_o - lamp_params.ring_width
+            res = [[z, r_i, r_o, lamp_idx]] + res
+
+        # bottom rings
+        z = 0
+        r_i = lamp_params.sphere_diameter / 2 - lamp_params.ring_width
+        while r_i > lamp_params.sphere_opening_bottom / 2:
+            z -= lamp_params.layer_thickness
+            r_o = np.sqrt((lamp_params.sphere_diameter / 2) ** 2 - z ** 2)
+            r_i = r_o - lamp_params.ring_width
+            res += [[z, r_i, r_o, lamp_idx]]
+
+        # convert to numpy array and round to integers
+        res = np.asarray(res)
+        res = np.round(res)
+
+        # correct inner diameters for sufficient overlap
+        i = (len(res) - 1) // 2
+        for ii in range(1, i):
+            middle = int(np.ceil((res[ii - 1, 2] + res[ii - 1, 1]) / 2))
+            res[ii, 1] = min(res[ii, 1], middle)
+        for ii in range(i + 1, len(res) - 1):
+            middle = int(np.ceil((res[ii + 1, 2] + res[ii + 1, 1]) / 2))
+            res[ii, 1] = min(res[ii, 1], middle)
+
+        lamp_arrays.append(res)
+
+    res = np.vstack(lamp_arrays)
     return res
 
 
-class data_linewidth_plot():
-    def __init__(self, x, y, **kwargs):
-        self.ax = kwargs.pop("ax", plt.gca())
-        self.fig = self.ax.get_figure()
-        self.lw_data = kwargs.pop("linewidth", 1)
-        self.lw = 1
-        self.fig.canvas.draw()
+def post_process_rings(rings: np.ndarray) -> np.ndarray:
+    r_os = np.unique(rings[:, 2])
+    for r_o in r_os:
+        idxs = rings[:, 2] == r_o
+        r_i = min(rings[idxs, 1])
+        rings[idxs, 1] = r_i
 
-        self.ppd = 72./self.fig.dpi
-        self.trans = self.ax.transData.transform
-        self.linehandle, = self.ax.plot([],[],**kwargs)
-        if "label" in kwargs: kwargs.pop("label")
-        self.line, = self.ax.plot(x, y, **kwargs)
-        self.line.set_color(self.linehandle.get_color())
-        self._resize()
-        self.cid = self.fig.canvas.mpl_connect('draw_event', self._resize)
-
-    def _resize(self, event=None):
-        lw =  ((self.trans((1, self.lw_data))-self.trans((0, 0)))*self.ppd)[1]
-        if lw != self.lw:
-            self.line.set_linewidth(lw)
-            self.lw = lw
-            self._redraw_later()
-
-    def _redraw_later(self):
-        self.timer = self.fig.canvas.new_timer(interval=10)
-        self.timer.single_shot = True
-        self.timer.add_callback(lambda : self.fig.canvas.draw_idle())
-        self.timer.start()
+    return rings
 
 
 def main():
-    sphere_diameter = 450
-    layer_thickness = 8
-    ring_width = 15
-    sphere_opening_top = 150
-    sphere_opening_bottom = 250
+    lamps = [
+        LampParams(
+            sphere_diameter=450,
+            layer_thickness=8,
+            ring_width=15,
+            sphere_opening_top=150,
+            sphere_opening_bottom=250
+        ),
+        LampParams(
+            sphere_diameter=450,
+            layer_thickness=8,
+            ring_width=15,
+            sphere_opening_top=150,
+            sphere_opening_bottom=250
+        ),
+        LampParams(
+            sphere_diameter=450,
+            layer_thickness=8,
+            ring_width=15,
+            sphere_opening_top=150,
+            sphere_opening_bottom=250
+        )
+    ]
 
-    rings = compute_rings(
-        sphere_diameter,
-        layer_thickness,
-        ring_width,
-        sphere_opening_top,
-        sphere_opening_bottom
-    )
+    rings = compute_rings(lamps)
+    rings = post_process_rings(rings)
 
-    print(rings[rings[:, 2].argsort()])
+    rings_sorted_ro = rings[rings[:, 2].argsort()]
+    print(rings_sorted_ro)
     print(f"#rings: {len(rings)}")
 
-    fig, ax = plt.subplots(1, 1)
-    plt.suptitle("cardboard lamp")
-    ax.axis('equal')
-    for z, r_i, r_o in rings:
-        data_linewidth_plot([r_i, r_o], [z, z], ax=ax, linewidth=layer_thickness, color='brown')
-        data_linewidth_plot([-r_i, -r_o], [z, z], ax=ax, linewidth=layer_thickness, color='brown')
-    plt.show()
+    # fig, ax = plt.subplots(1, 1)
+    # plt.suptitle("cardboard lamp")
+    # ax.axis('equal')
+    # for z, r_i, r_o in rings:
+    #     data_linewidth_plot([r_i, r_o], [z, z], ax=ax, linewidth=layer_thickness, color='brown')
+    #     data_linewidth_plot([-r_i, -r_o], [z, z], ax=ax, linewidth=layer_thickness, color='brown')
+    # plt.show()
 
 
 if __name__ == "__main__":
